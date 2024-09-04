8b5b64f4c4f70d40bd4fa806ecd05e388cc64788
rleiva-2019154 <126587929+rleiva-2019154@users.noreply.github.com>
2024-09-04 11:50:34 -0600

    Realizando el login y register con validaciones para que no envie ningun campo vacio

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateJwt } from "../../utils/generate-jwt.js";
 import User from "../users/user.model.js";
 import bcryptjs from 'bcryptjs';
 
@@ -27,5 +28,75 @@ export const createDefaultBossUser = async () => {
         }
     } catch (err) {
         console.error('Error al crear el usuario BOSS por defecto', err);
+    }
+};
+
+// Función de registro (declarativa)
+export const register = async (req, res) => {
+    try {
+        const data = req.body;
+
+        // Asegura que el rol sea siempre 'EMPLOYEE'
+        data.role = 'EMPLOYEE';
+
+        const salt = await bcryptjs.genSalt();
+        data.password = await bcryptjs.hash(data.password, salt);
+        
+        const user = new User(data);
+        await user.save();
+
+        return res.send({
+            message: `Usuario agregado a la base de datos correctamente, inicia sesión con el correo ${user.email}`,
+            userDetails: {
+                user: user.username,
+                email: user.email
+            }
+        });
+    } catch (err) {
+        console.error('Error al registrar usuario', err);
+        return res.status(500).send({ message: 'No se pudo registrar al usuario, intenta de nuevo más tarde', err });
+    }
+};
+
+// Función de inicio de sesión
+export const login = async (req, res) => {
+    const { username, email, password } = req.body;
+    const lowerEmail = email ? email.toLowerCase() : null;
+    const lowerUsername = username ? username.toLowerCase() : null;
+    try {
+        const userExist = await User.findOne({
+            $or: [
+                { username: lowerUsername },
+                { email: lowerEmail }
+            ]
+        });
+        if (!userExist) {
+            return res.status(404).json({
+                msg: 'Credenciales inválidas',
+                error: 'Aún no tienes cuenta con nosotros'
+            });
+        }
+        const checkPassword = await bcryptjs.compare(password, userExist.password);
+        if (!checkPassword) {
+            return res.status(403).json({
+                msg: 'Credenciales inválidas',
+                error: 'Contraseña incorrecta'
+            });
+        }
+
+        const token = await generateJwt({ uid: userExist._id, email: userExist.email });
+        return res.json({
+            msg: 'Inicio de sesión exitoso',
+            userDetails: {
+                username: userExist.username,
+                email,
+                role: userExist.role,
+                token,
+                uid: userExist._id
+            }
+        });
+    } catch (err) {
+        console.error('Error al iniciar sesión', err);
+        return res.status(500).send({ message: 'Error al iniciar sesión, intenta de nuevo más tarde', err });
     }
 };
