8e82aa779b4e6063f8baa225a8439bd0f34fd8cc
rleiva-2019154 <126587929+rleiva-2019154@users.noreply.github.com>
2024-09-10 14:36:34 -0600

    Agrego una nueva validacion donde verifique que el usuario que esta solicitando vacaciones sea boss o employee

--- src/vacationRequest/vacationRequest.routes.js
+++ src/vacationRequest/vacationRequest.routes.js
@@ -3,11 +3,11 @@ import { validateJwt } from "../../middlewares/validate_Jwt.js";
 import {
     addVacations
 } from "./vacationRequest.controller.js"
-import { isAdmin, isBoss, isEmployee } from "../../middlewares/role-auth.js";
+import { isAdmin, isBoss, isEmployee, isBossOrEmployee } from "../../middlewares/role-auth.js";
 import { addVacationRequestValidator } from "../../middlewares/check-validators.js";
 
 const api = Router()
 
-api.post('/addVacations', validateJwt, isEmployee, isBoss, addVacationRequestValidator, addVacations)
+api.post('/addVacations', validateJwt, isBossOrEmployee, addVacationRequestValidator, addVacations)
 
 export default api
