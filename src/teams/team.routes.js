1d45d199b8523d5c9ed4212e23b99356e120bbe5
rleiva-2019154 <rleiva-2019154@kinal.edu.gt>
2024-09-20 13:15:04 -0600

    Creacion de todas las funciones del modelo Team, create, add member, edit team, edit team boss, remove member, delete team, get teams, get team by id

---
+++ src/teams/team.routes.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import { validateJwt } from "../../middlewares/validate_Jwt.js";
+import { 
+    createTeam,
+    addMemberToTeam,
+    editTeam,
+    editTeamBoss,
+    removeMemberFromTeam,
+    deleteTeam,
+    getTeams,
+    getTeamById
+} from "./team.controller.js"
+import { isAdmin, isBoss, isBossOrEmployee} from "../../middlewares/role-auth.js";
+import { 
+    addTeamValidator, 
+    editTeamValidator
+} from "../../middlewares/check-validators.js";
+
+
+const api = Router()
+
+api.post('/createTeam', validateJwt, isAdmin, addTeamValidator, createTeam)
+
+api.put('/addMemberToTeam/:teamId', validateJwt, isAdmin, addMemberToTeam)
+
+api.put('/editTeam/:teamId', validateJwt, isAdmin, editTeamValidator, editTeam)
+
+api.put('/editTeamBoss/:teamId', validateJwt, isBoss, editTeamBoss)
+
+api.delete('/removeMemberFromTeam/:teamId', validateJwt, isAdmin, removeMemberFromTeam)
+
+api.delete('/deleteTeam/:teamId', validateJwt, isAdmin,deleteTeam)
+
+api.get('/getTeams', validateJwt, isAdmin, getTeams)
+
+api.get('/getTeamById/:teamId', validateJwt, isAdmin, getTeamById)
+
+export default api
+
